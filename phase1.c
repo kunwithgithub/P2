369bf1336ca105448dac721bf4a84032dc16d6bc
kunwithgithub <45131670+kunwithgithub@users.noreply.github.com>
2019-04-23 12:06:04 -0700

    Update phase1.c

--- phase1.c
+++ phase1.c
@@ -1,16 +1,20 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "queue.h"
+
+struct node {
+  void *data;
+  struct node *next;
+
+};
 
 struct queue {
 	/* TODO Phase 1 */
-	void *data;
 	int length;
-	struct queue *front;
-	struct queue *back;
-//	struct queue *next; 
+	struct node *front;
+	struct node *back;
+
 };
 
 
@@ -18,77 +22,101 @@ struct queue {
 queue_t queue_create(void)
 {
 	/* TODO Phase 1 */
-	
-		
-	struct queue *head = (struct queue*)malloc(sizeof(struct queue));
-	head->front=NULL;
-	head->back=NULL;
-	head->next=NULL;
-	head->data=NULL;
-	head->length=0;
-	return head;	
+  struct queue *myQueue = (struct queue*)malloc(sizeof(struct queue));
+	myQueue->front = NULL;
+  myQueue->back = NULL;
+	myQueue->length = 0;
+	return myQueue;	
 }
 
 int queue_destroy(queue_t queue)
 {
 	/* TODO Phase 1 */
+  if(queue == NULL){
+    return -1;
+  }
 
 }
 
 int queue_enqueue(queue_t queue, void *data)
 {
 	/* TODO Phase 1 */
-	struct queue *newNode = (struct queue*)malloc(sizeof(struct queue));
-	if(newNode == NULL){
-		return -1;
-	}
-	if(queue->length == 0){
-		
-	}
-	newNode->data = data;
-	queue->front;
-	newNode->length = queue->length+1;
-	queue->back = newNode;
+  struct node *newNode = (struct node*)malloc(sizeof(struct node));
+  if(data == NULL||queue == NULL||newNode == NULL){
+    return -1;
+  }
 	
+  newNode->data = data;
+  newNode->next = NULL;
+  if(queue->length == 0){
+      queue->front = newNode;
+      queue->back = newNode;
+      return 0;
+  }
+  queue->back->next = newNode;
+  queue->back = newNode;
+	
+  return 0;
 }
 
 int queue_dequeue(queue_t queue, void **data)
 {
 	/* TODO Phase 1 */
+  if(queue->length == 0 || queue == NULL || queue->front->data == NULL){
+    return -1;
+  }
+  struct node *frontNode = queue->front;
+  queue->front = queue->front->next;
+  *data = frontNode->data;
+  frontNode = NULL;
+  queue->length--;
+
+  return 0;
 }
 
 int queue_delete(queue_t queue, void *data)
 {
 	/* TODO Phase 1 */
-	int length=queue->length;
-	struct queue *head=queue->front;
+	int length = queue->length;
+  if(queue->length == 0||queue == NULL|| data == NULL){
+    return -1;
+  }
+	struct node *head = queue->front;
+  if(head->data == data){
+    queue->front = head->next;
+    free(head);
+    head = NULL;
+  }
 	for(int i=0;i<length;i++){
-		if(head==NULL || head->data=NULL){
-			return -1;
-		}
-		if(head->data=data){
-			free(head->data);
-			head->data=NULL;
-		}
-	}
-	return 0;
+		if(head->next->data == data){
+      struct node *willbedelete = head->next;
+      head->next = head->next->next;
+      free(willbedelete);
+      willbedelete = NULL;
+      return 0;
+    }
+
+		
+    head = head->next;
+  }
+	return -1;
 }
 
 int queue_iterate(queue_t queue, queue_func_t func, void *arg, void **data)
 {
 	/* TODO Phase 1 */
-	struct queue *head=queue->front;
+	struct node *head=queue->front;
 	while(head!=NULL){
 		int test=func(head->data,arg);
 		if(test==-1){
-			*data = head;
+			*data = head->data;
 			return -1;
 		}
 		if(head->next!=NULL){
 			head=head->next;
 		}
 	}
-	return 0;
+	return queue->length;
 	
 }
 
