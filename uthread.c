8b6f3433118c3922894050515cf6a85f5ae3ee4e
kunwithgithub <45131670+kunwithgithub@users.noreply.github.com>
2019-04-27 17:21:44 -0700

    Update uthread.c

--- uthread.c
+++ uthread.c
@@ -17,45 +17,57 @@
 enum state{READY,RUNNING,BLOCK,ZOMBIE};
 
 
-static uthread_t TID;
-static queue_t ready, running, block, zombie;
+uthread_t TID;
+
+queue_t ready, block, zombie;
+//queue_t running
 
 struct Thread{
   uthread_t tid;
   void *stackPtr;
   enum state state_of_uthread;
-  char stack[STACK_SIZE];
+//char stack[STACK_SIZE];
   ucontext_t ctx;
 };
+
+Thread *currentRunningThread;
 
 void uthread_yield(void)
 {
 	/* TODO Phase 2 */
   struct Thread *from;
   struct Thread *to;
-  queue_dequeue(running,(void *)from);
-  queue_enqueue(ready,to);
-  uthread_ctx_switch((void *)from, (void *)to);
-  from->state_of_uthread = RUNNING;
-  queue_enqueue(running, to);
 
+  queue_dequeue(ready,(void*)to);
+  from = currentRunningThread;
+
+  uthread_ctx_switch((void *)from, (void *)to);
+  currentRunningThread = to;
+  currentRunningThread->state_of_uthread = RUNNING;
+  queue_enqueue(ready, (void*)from);
+//  queue_enqueue(running, (void *)to);
 }
 
 uthread_t uthread_self(void)
 {
 	/* TODO Phase 2 */
-
-  
-
+  if(currentRunningThread == NULL){
+  struct Thread *to;
+  queue_dequeue(ready,(void *)to);
+  uthread_ctx_switch((void *)currentRunningThread, (void *)to);
+  to->state_of_uthread = RUNNING;
+  currentRunningThread = to;
+//  queue_enqueue(running, to);
+  }
+  return currentThread->tid;
 }
 
 int uthread_create(uthread_func_t func, void *arg)
 {
 	/* TODO Phase 2 */
-  
   if(TID == 1){
     ready = queue_create();
-    running = queue_create();
+  //  running = queue_create();
     block = queue_create();
     zombie = queue_create();
   }
@@ -63,13 +75,13 @@ int uthread_create(uthread_func_t func, void *arg)
   if(newThread == NULL){
     return -1;
   }
-  newThread->stackPtr = NULL;
+  newThread->stackPtr = uthread_ctx_alloc_stack();
   newThread->state_of_uthread = READY;
   newThread->tid = TID;
-  newThread->ctx = NULL;
-  
+  newThread->ctx;
+//newThread->ctx = NULL;
 
-  int ctxInitializationSuccess = uthread_ctx_init(newThread->ctx, newThread->stackPtr, func, arg);
+  int ctxInitializationSuccess = uthread_ctx_init(&(newThread->ctx), newThread->stackPtr, func, arg);
 
   queue_enqueue(ready, newThread);
   TID++;
@@ -84,5 +96,16 @@ void uthread_exit(int retval)
 int uthread_join(uthread_t tid, int *retval)
 {
 	/* TODO Phase 2 */
+  if(tid == 0){
+    return -1
+  }
+  while(1){
+    int length = queue_length(ready);
+    if(length == 0){
+      break;
+    }
+    uthread_yield();
+  }
+  return 0;
 	/* TODO Phase 3 */
 }
