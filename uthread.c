df430701c441474e05c1834612fc8675f73d5f44
kunwithgithub <45131670+kunwithgithub@users.noreply.github.com>
2019-04-28 23:53:22 -0700

    Update uthread.c

--- uthread.c
+++ uthread.c
@@ -26,7 +26,7 @@ struct Thread{
   uthread_t tid;
   void *stackPtr;
   enum state state_of_uthread;
-  int isMainThread;
+
 //char stack[STACK_SIZE];
   uthread_ctx_t ctx;
 };
@@ -36,22 +36,15 @@ struct Thread *currentRunningThread;
 void uthread_yield(void)
 {
 	/* TODO Phase 2 */
-  printf("yield Start\n");
+ 
+  queue_enqueue(ready, (void *)currentRunningThread); 
   struct Thread *from;
   struct Thread *to;
-
-  queue_dequeue(ready,(void*)to);
   from = currentRunningThread;
-  printf("main thread is 1: so %d\n",from->isMainThread);
-  printf("main thread is 1: so %d\n",to->isMainThread);
-
-  uthread_ctx_switch(&(from->ctx),&(to->ctx));
-  printf("yield Progress\n");
+  queue_dequeue(ready, (void**)&to);
   currentRunningThread = to;
-  currentRunningThread->state_of_uthread = RUNNING;
-  queue_enqueue(ready, (void*)from);
-  printf("yield Finished\n");
-//  queue_enqueue(running, (void *)to);
+  uthread_ctx_switch(&(from->ctx), &(currentRunningThread->ctx));
+
 }
 
 uthread_t uthread_self(void)
@@ -65,21 +58,22 @@ int uthread_create(uthread_func_t func, void *arg)
 {
 	/* TODO Phase 2 */
   printf("create begin\n");
+ struct Thread *mainThread = malloc(sizeof(struct Thread));
   if(TID == 0){
     ready = queue_create();
   //  running = queue_create();
     block = queue_create();
     zombie = queue_create();
-    struct Thread *mainThread = (struct Thread*)malloc(sizeof(struct Thread));
-    mainThread->isMainThread = 1;
+   
+	mainThread->tid = TID;
+    mainThread->state_of_uthread = RUNNING;
     currentRunningThread = mainThread;
-    currentRunningThread->state_of_uthread = RUNNING;
-
-    printf("main Thread is running and tid is %d \n",uthread_self());
-    
+    currentRunningThread->stackPtr = uthread_ctx_alloc_stack();
+	 	 
+    TID++;
   }
   
-  struct Thread *newThread = (struct Thread*)malloc(sizeof(struct Thread));
+  struct Thread *newThread = malloc(sizeof(struct Thread));
   if(newThread == NULL){
     return -1;
 
@@ -87,32 +81,26 @@ int uthread_create(uthread_func_t func, void *arg)
   newThread->stackPtr = uthread_ctx_alloc_stack();
   newThread->state_of_uthread = READY;
   newThread->tid = TID;
-  newThread->isMainThread = 0;
+ 
 //newThread->ctx = NULL;
 
-  int ctxInitializationSuccess = uthread_ctx_init(&(newThread->ctx), newThread->stackPtr, func, arg);
-  printf("context init %d\n",ctxInitializationSuccess);
+  uthread_ctx_init(&(newThread->ctx), newThread->stackPtr, func, arg);
 
+ 
   queue_enqueue(ready, newThread);
-  uthread_ctx_switch(&(currentRunningThread->ctx),&(newThread->ctx));
-
+//  uthread_yield();
   TID++;
-  printf("create end\n");
+
   return newThread->tid;
 }
 
 void uthread_exit(int retval)
 {
 	/* TODO Phase 2 */
-  /*if(currentRunningThread->state_of_uthread == ZOMBIE){
-    uthread_ctx_destroy_stack(currentRunningThread->stackPtr);
-  }*/
-  if(retval > 0){
-    uthread_ctx_destroy_stack(currentRunningThread->stackPtr);
-    free(currentRunningThread);
-  }else{
-    queue_enqueue(zombie,(void *)currentRunningThread);
-  }
+	struct Thread *temp = currentRunningThread;
+	queue_enqueue(zombie,temp);
+	uthread_yield();
+	temp->state_of_uthread = ZOMBIE;
 
 }
 
@@ -125,10 +113,11 @@ int uthread_join(uthread_t tid, int *retval)
   while(1){
     int length = queue_length(ready);
     if(length == 0){
+      printf("length is %d\n",length);
       break;
     }
     uthread_yield();
-    printf("ggwp\n");
+ 
   }
   return 0;
 	/* TODO Phase 3 */
