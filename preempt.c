8aafa0bad3ff4f7d1385397bd4efc15b7fac2d69
kunwithgithub <45131670+kunwithgithub@users.noreply.github.com>
2019-05-01 20:44:40 -0700

    Update preempt.c

--- preempt.c
+++ preempt.c
@@ -15,17 +15,69 @@
  */
 #define HZ 100
 
+ struct sigaction sa;
+ struct itimerval timer;
+
+void timer_handle (int signum)
+{
+ static int count = 0;
+ printf ("timer expired %d times\n", ++count);
+}
+
 void preempt_disable(void)
 {
 	/* TODO Phase 4 */
+  /* Reference: */
+  sigset_t sigprof;
+  sigemptyset(&sigprof);
+  sigaddset(&sigprof,SIGPROF);
+
+  if(sigprocmask(SIG_BLOCK, &sigprof, NULL) == -1){
+
+    perror("sigprocmask");
+    abort();
+  }
+
 }
 
 void preempt_enable(void)
 {
 	/* TODO Phase 4 */
+    sigset_t sigprof;
+    sigemptyset(&sigprof);
+    sigaddset(&sigprof,SIGPROF);
+    if(sigprocmask(SIG_UNBLOCK, &sigprof, NULL) == -1){
+      perror("sigprocmask");
+      abort(); 
+    }
+
 }
 
 void preempt_start(void)
 {
 	/* TODO Phase 4 */
+  /*
+ * preempt_start - Start thread preemption
+ *
+ * Configure a timer that must fire a virtual alarm at a frequency of 100 Hz and
+ * setup a timer handler that forcefully yields the currently running thread.
+ */
+/* Install timer_handler as the signal handler for SIGVTALRM. */
+ memset (&sa, 0, sizeof (sa));
+ sa.sa_handler = &timer_handle;
+ sigaction (SIGVTALRM, &sa, NULL);
+
+ /* Configure the timer to expire after 250 msec... */
+ timer.it_value.tv_sec = 0;
+ timer.it_value.tv_usec = 1000000/HZ;
+ /* ... and every 250 msec after that. */
+ timer.it_interval.tv_sec = 0;
+ timer.it_interval.tv_usec = 1000000/HZ;
+
+
+ setitimer (ITIMER_VIRTUAL, &timer, NULL);
+
+
+
+
 }
